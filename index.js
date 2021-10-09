21eb397a201ed8431ccd6e4012c4994f3571c82a
Jahson Diego <jdiego847@gmail.com>
2021-10-08 21:02:16 -0400

    commit tree 9386a51d9ce9fe1496e3c3c4aeda97bfb331cd6c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(vibes){
+    if (vibes === undefined) {vibes = "roller-skate";}
+    return `This Saturday, I want to ${vibes}!`
+}
+function mondayWork(chore){
+    if (chore === undefined) {chore = "go to the office";}
+    return `This Monday, I will ${chore}.`;
+}
+function wrapAdjective(adject){
+    const part1 = "You are";
+    return function(result){
+        if (adject === undefined) { result = "special";}
+        return `${part1} ${adject}${result}${adject}!`;
+    }
+}
+console.log(wrapAdjective("%")("21 years old"))
